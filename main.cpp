3b00abce12ef9e26b2c462928f01485e870889f8
Mtendekuyokwa19 <mtendekuyokwa19@gmail.com>
2023-03-17 16:56:28 +0200

    add file

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int yandhi(int y, int x){
+	
+	
+	int sum;
+	sum= x+y;
+	
+	
+	
+}
+
+int a=yandhi(3,4);
+
+int main( ) {
+	
+cout<<a;
+	return 0;
+}
+
+
