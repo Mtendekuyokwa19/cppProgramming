9ee52b706477c33896733156029cd1058fd9380f
Mtendekuyokwa19 <mtendekuyokwa19@gmail.com>
2023-03-22 06:43:07 +0200

    a cpp app that checks for how much you are paid

--- exercise3.cpp
+++ exercise3.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 using namespace std;
-
-
-int main( ) {
-
-if (4>90||6==6){
-
-cout<<"Lebron nsiku";
-}
-else {
-
-cout<<"skrr skrr";
-}
-return 0;
+int main (){
+	
+	float a;
+	float tax;
+	
+	cout<<"enter your salary";
+	cin>>a;
+	
+	if (a>=0 && a<=100000){
+		
+		
+		cout<<"no tax";
+		
+		
+	}
+ if (a>=100001&&a<=150000){
+		
+	tax= 0.15 * a;	
+		
+		cout<<tax;	
+		
+	}
+	
+ if (a>=150001&&a<=1000000){
+		
+		tax=0.25*a;
+		cout<<tax;
+	}
+	else{
+		
+		
+		tax=0.3*a;
+	}
+	
+	
+	return 0;
 }
