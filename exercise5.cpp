9bf600331b96511e4abc76665be58b28d41baafc
Mtendekuyokwa19 <mtendekuyokwa19@gmail.com>
2023-03-28 20:28:51 +0200

    m

--- exercise5.cpp
+++ exercise5.cpp
@@ -1,1 +1,37 @@
 
+
+#include <iostream>
+using namespace std;
+int main() {
+
+  int miles;
+  int amount;
+  int average;
+  cout << "enter the miles for the rented vehicle ";
+  cin >> miles;
+  if (miles <= 100 && miles > 0) {
+    amount = miles * 300;
+
+    average = amount / miles;
+    cout << "the amount for the journey is Mk " << amount
+         << " the average cost of each mile  is Mk" << average << "/mile";
+
+  } else if (miles > 100 && miles <= 300) {
+    amount = (miles - 100) * 200 + (100 * 300);
+
+    average = amount / miles;
+    cout << "the amount for the journey is Mk " << amount
+         << " the average cost of each mile  is Mk" << average << "/mile";
+  } else if (miles > 300) {
+    amount = (miles - 300) * 150 + (200 * 200) + (100 * 300);
+
+    average = amount / miles;
+    cout << "the amount for the journey is Mk " << amount
+         << " the average cost of each mile  is Mk" << average << "/mile";
+  } else {
+
+    cout << " enter the right credintials please";
+  }
+
+  return 0;
+}
